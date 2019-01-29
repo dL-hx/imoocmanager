4d6e6861fc0f9c6e84fa07dd07cc6da323057221
lenovo222 <897057246@qq.com>
2019-01-29 20:03:52 +0800

    33

--- src/common.js
+++ src/common.js
@@ -3,6 +3,7 @@ import React from "react";
 import {Row, Col} from "antd";
 import Header from "./components/Header";
 import './style/common.less';
+
 export default class Common extends React.Component {
   render() {
     return (
